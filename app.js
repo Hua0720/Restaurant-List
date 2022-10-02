8ac3ba74110577b8b5925f15055e56b7d062cfe9
Kris <113756039+Hua0720@users.noreply.github.com>
2022-10-03 00:21:48 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// 載入express、express-handlebars、json
+const express = require('express')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const app = express()
+
+// 設定預設port
+const port = 3000
+
+// 設定handlebars引擎
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// 設定靜態檔案位置
+app.use(express.static('public'))
+
+// 設定路由
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+// 使用params 製作動態路由
+app.get('/restaurants/:id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
+  res.render('show', { restaurant: restaurant })
+})
+
+// 搜尋功能
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim()
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+      restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+// 設置監聽器
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`)
+})
